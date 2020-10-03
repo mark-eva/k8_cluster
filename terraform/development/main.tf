e9de464b05091d4b5ce9aad09de6df505c15f96d
root <root@LAPTOP-IH887QOJ.localdomain>
2020-10-03 10:59:00 +0100

    created the first version of k8 architecture

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -32,7 +32,7 @@ module "dev_subnet" {
 
 
 
-# Code that creates master and node
+# Code that creates master and slave nodes
 
 module "master" {
   source          = "../modules/vm"
@@ -65,49 +65,52 @@ resource "aws_key_pair" "deployer" {
   public_key = file("/mnt/c/Main/08_keys/public_key.pub")
 }
 
-/*
-#Add an elastic IP to the vms
-resource "aws_eip" "db1" {
-  instance = module.db1.vm_id
+
+# Add an elastic IP to the vms
+resource "aws_eip" "master" {
+  instance = module.master.vm_id
   vpc      = true
 }
 
-resource "aws_eip" "db2" {
-  instance = module.db2.vm_id
+resource "aws_eip" "node_1" {
+  instance = module.node_1.vm_id
   vpc      = true
 }
 
-*/
-/*
-#Set up an internet gateway to route to your VPC
-resource "aws_internet_gateway" "poc_gateway" {
-  vpc_id = module.poc_vpc.vpc_id
+resource "aws_eip" "node_2" {
+  instance = module.node_2.vm_id
+  vpc      = true
 }
 
 
-#Create a route table 
 
-resource "aws_route_table" "poc_route_table" {
-  vpc_id = module.poc_vpc.vpc_id
+# Set up an internet gateway to route to your VPC
+resource "aws_internet_gateway" "k8gateway" {
+  vpc_id = module.dev_vpc.vpc_id
+}
+
+
+# Create a route table 
+
+resource "aws_route_table" "dev_route_table" {
+  vpc_id = module.dev_vpc.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.poc_gateway.id
+    gateway_id = aws_internet_gateway.k8gateway.id
   }
 }
 
+# Associate subnet to the route table  
 
-resource "aws_route_table_association" "poc_subnet_to_routetable" {
-  subnet_id      = module.poc_subnet.subnet_id
-  route_table_id = aws_route_table.poc_route_table.id
-}
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("/mnt/c/Main/08_keys/public_key.pub")
+resource "aws_route_table_association" "dev_subnet_to_routetable" {
+  subnet_id      = module.dev_subnet.subnet_id
+  route_table_id = aws_route_table.dev_route_table.id
 }
 
 
+/*
 # generate inventory file for Ansible
 resource "local_file" "ansible_conf" {
   content = templatefile("../../../ansible/templates/hosts.tpl",
